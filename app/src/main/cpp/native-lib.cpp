e100affe91a7ed94552f0541c2c4c5f39464d0e0
wangtao <wangtao@ai-beings.com>
2024-07-11 14:28:30 +0800

    add .so .a files

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,16 +1,24 @@
 #include <jni.h>
 #include <string>
 
+// 用于链接 C 库的头文件
+extern "C" {
+#include "two.h"
+#include "one.h"
+}
+
 extern "C" JNIEXPORT jstring JNICALL Java_com_example_hellondk_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    std::string hello = "Hello from C++ ";
+    std::string result = hello + std::to_string(numReduce(19, 7));
+    return env->NewStringUTF(result.c_str());
 }
 
 extern "C" JNIEXPORT jstring JNICALL Java_com_example_hellondk_MainActivity_stringFromJNI2(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++ 2";
-    return env->NewStringUTF(hello.c_str());
+    std::string hello = "Hello from C++ 2 ";
+    std::string result = hello + std::to_string(numAdd(19, 7));
+    return env->NewStringUTF(result.c_str());
 }
